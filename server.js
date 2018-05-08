33354172aedc2820fc1bbe4b241a1b803771888c
Glad Chinda <gladxeqs@gmail.com>
2018-05-08 16:45:27 +0100

    Initialized server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const cors = require('cors');
+const next = require('next');
+const Pusher = require('pusher');
+const logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev });
+const handler = app.getRequestHandler();
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  encrypted: true
+});
+
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.use(cors());
+    server.use(logger('dev'));
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+
+    server.get('*', (req, res) => {
+      return handler(req, res);
+    });
+
+    server.listen(port, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
